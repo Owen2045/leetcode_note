7c16e95d2c7c145c12b185238611d7cdafde1c76
owen <owen.li@acercsi.com>
2026-01-30 16:42:28 +0800

    再刷

---
+++ 3. Roman to Integer/01-2026.01.30/roman_to_integer.py
@@ -0,0 +1,23 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        rm_map = {
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000
+        }
+
+        ans = 0
+        now_num = 0
+        for o in reversed(s):
+            i = rm_map[o]
+            if i < now_num:
+                ans -= i
+            else:
+                ans += i
+            now_num = i
+
+        return ans
